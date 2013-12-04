f69e66428734c7d34b1d618943c450a5ab9d2783
J.F. Omhover <jf.omhover@gmail.com>
2013-12-04 17:56:27 +0100

    corrected code + debug mode
    - corrected a total failure of copy/paste (sorry)
    - added (restored) a light debug mode to display values via Serial for
    testing purpose

--- experiments_integrated/ardusat_magfield3/ardusat_magfield3.ino
+++ experiments_integrated/ardusat_magfield3/ardusat_magfield3.ino
@@ -1,53 +1,4 @@
 /*
-    File :         ardusat_magfield3.ino (simplified)
-    Author :       Jean-Francois Omhover (jf.omhover@gmail.com, twitter:@jfomhover)
-    Last Changed : Dec. 3rd 2013, clean-up/simplified version of the ardusat_configurablepuller for pulling MAG values only
-    Description :  configurable code for pulling the RAW VALUES (int) of the MAGNETOMETER regularly
-                   and returning binary values back to earth
-
-    Usage :        - compile, and launch
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
-#define PULL_DELAY  22000        // data is pooled every PULL_DELAY seconds
-
-
-// ***************
-// *** HEADERS ***
-// ***************
-
-#include <Arduino.h>
-#include <Wire.h>//for I2C
-#include <nanosat_message.h>
-#include <I2C_add.h>
-#include <I2C_Conv.h>  // for SAT_Geiger
-
-#include <EEPROM.h>
-#include <OnboardCommLayer.h>
-#include <SAT_AppStorage.h>
-
-#include <SAT_Mag.h>
-#include <util/crc16.h>
-
-// ************************
-// *** API CONSTRUCTORS ***
-// ************************
-
-SAT_AppStorage store;
-SAT_Mag mag;/*
     File :         ardusat_magfield3.ino
     Author :       Jean-Francois Omhover (jf.omhover@gmail.com, twitter:@jfomhover)
     Last Changed : Dec. 3rd 2013, clean-up
@@ -83,8 +34,8 @@ SAT_Mag mag;/*
 // *** "PUBLIC" CONFIG ***
 // ***********************
 
-#define PULL_DELAY  1000        // data is pooled every PULL_DELAY seconds
-
+#define PULL_DELAY  22000        // data is pooled every PULL_DELAY seconds
+#define DEBUG_MODE              // uncomment to test the code (values sent on serial instead)
 
 // ***************
 // *** HEADERS ***
@@ -109,104 +60,6 @@ SAT_Mag mag;/*
 
 SAT_AppStorage store;
 SAT_Mag mag;
-
-
-// ********************************
-// *** SENSOR POOLING FUNCTIONS ***
-// ********************************
-
-#define AVAILABLE_SAT_LUM         0x01
-#define AVAILABLE_SAT_MAG         0x02
-#define AVAILABLE_SAT_TMP         0x04
-#define AVAILABLE_SAT_INFRATHERM  0x08
-#define AVAILABLE_SAT_ACCEL       0x10
-#define AVAILABLE_SAT_GYRO        0x20
-#define AVAILABLE_SAT_GEIGER      0x40
-#define AVAILABLE_CRC16           0x80
-
-struct _dataStruct {
-  char header;          // don't touch that, or adapt it, whatever ^^
-  byte availableValues;
-  long int ms;          // i use that as an id
-
-  int16_t mag_values[3];
-  uint16_t  crc16;
-} data;
-
-#define DATA_LENGTH  sizeof(struct _dataStruct)
-
-// blanks everything (to be REALLY sure)
-void resetValues() {
-  byte * t_bytePtr = (byte *)&data;
-  for (int i=0; i<DATA_LENGTH; i++) {
-    t_bytePtr[i] = 0;
-  }
-}
-
-// pools the values needed by the experiment
-// here : the magnetometer
-void poolValues() {
-  resetValues();
-
-  data.header = '#';            // used for parsing (even if the struct has a fixed size)
-
-  data.availableValues = AVAILABLE_SAT_MAG | AVAILABLE_CRC16;
-
-  data.ms = millis();
-
-  data.mag_values[0] = mag.readx();
-  data.mag_values[1] = mag.ready();
-  data.mag_values[2] = mag.readz();
-
-  uint8_t * ptr_t = (uint8_t *)&data;
-  uint8_t size_t = sizeof(struct _dataStruct) - 2;
-  data.crc16 = 0;
-  for (int i=0; i<size_t; i++)
-    data.crc16 = _crc16_update(data.crc16,ptr_t[i]);
-}
-
-
-// ******************
-// *** MAIN SETUP ***
-// ******************
-
-void setup()
-{
-  mag.configMag();          // turn the MAG3110 on
-}
-
-
-// *****************
-// *** MAIN LOOP ***
-// *****************
-
-signed long int previousMs = 0;
-signed long int nextMs = 0;
-int dataSent = 0;
-
-void loop()
-{
-
-  nextMs = PULL_DELAY-(millis()-previousMs);  // "intelligent delay" : just the ms needed to have a perfect timing, takes into account the delay of all the functions in the loop
-  previousMs += PULL_DELAY;
-  
-  if (nextMs > 0)
-    delay(nextMs); //wait for next pull
-  else
-    previousMs += PULL_DELAY; // we missed a beat, skip one to get back on regular track
-
-  poolValues();   // pool the values needed
-
-  byte * t_bytePtr = (byte *)&data;
-  store.send(t_bytePtr, 0, DATA_LENGTH);   // sends data into the communication file and queue for transfer
-                                           // WARNING : introduces a 100ms delay
-  dataSent += DATA_LENGTH;
-  
-  if ((dataSent + DATA_LENGTH) > 10240) {
-    // TODO : close experiments, clean mode
-  }
-}
-
 
 
 // ********************************
@@ -263,6 +116,31 @@ void poolValues() {
     data.crc16 = _crc16_update(data.crc16,ptr_t[i]);
 }
 
+#ifdef DEBUG_MODE
+void debugValues() {
+  Serial.print("header=");
+  Serial.print(data.header);
+  Serial.print(", ms=");
+  Serial.print(data.ms);
+  Serial.print(", magX=");
+  Serial.print(data.mag_values[0]);
+  Serial.print(", magY=");
+  Serial.print(data.mag_values[1]);
+  Serial.print(", magZ=");
+  Serial.print(data.mag_values[2]);
+  Serial.print(", crc=");
+  Serial.println(data.crc16);
+
+  Serial.print("message(bin): ");
+  byte * t_bytePtr = (byte *)&data;
+  for (int i=0; i<DATA_LENGTH; i++) {
+    if (t_bytePtr[i]<0x10) {Serial.print("0");}
+    Serial.print(t_bytePtr[i],HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+}
+#endif
 
 // ******************
 // *** MAIN SETUP ***
@@ -271,6 +149,10 @@ void poolValues() {
 void setup()
 {
   mag.configMag();          // turn the MAG3110 on
+
+#ifdef DEBUG_MODE
+  Serial.begin(9600);
+#endif
 }
 
 
@@ -295,10 +177,19 @@ void loop()
 
   poolValues();   // pool the values needed
 
+
+#ifndef DEBUG_MODE
   byte * t_bytePtr = (byte *)&data;
   store.send(t_bytePtr, 0, DATA_LENGTH);   // sends data into the communication file and queue for transfer
                                            // WARNING : introduces a 100ms delay
   dataSent += DATA_LENGTH;
+#else
+  debugValues();
+  dataSent += DATA_LENGTH;
+  Serial.print("total data sent = ");
+  Serial.println(dataSent);
+#endif
+
   
   if ((dataSent + DATA_LENGTH) > 10240) {
     // TODO : close experiments, clean mode
